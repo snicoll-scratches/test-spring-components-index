15a23e5bade90804957bda39b0eac83cea30f224
Stephane Nicoll <snicoll@pivotal.io>
2016-08-17 13:51:31 +0200

    Add sample app

---
+++ sample-app/src/main/java/com/example/sample/SampleApp.java
@@ -0,0 +1,13 @@
+package com.example.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApp.class, args);
+	}
+
+}
---
+++ sample-app/src/main/java/com/example/sample/domain/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Dummy package-info for JPA stuff.
+ */
+package com.example.sample.domain;
