f86f1fec2e145f0a82b85c7790465e8ce0a4d343
Stephane Nicoll <snicoll@pivotal.io>
2016-08-30 17:01:13 +0200

    Add more scanning use cases

--- sample-app/src/main/java/com/example/sample/SampleApp.java
+++ sample-app/src/main/java/com/example/sample/SampleApp.java
@@ -2,8 +2,10 @@ package com.example.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class SampleApp {
 
 	public static void main(String[] args) {
--- sample-app/src/main/java/com/example/sample/service/SampleService.java
+++ sample-app/src/main/java/com/example/sample/service/SampleService.java
@@ -1,7 +1,18 @@
 package com.example.sample.service;
 
+import com.example.sample.domain.SpeakerRepository;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class SampleService {
+
+	private final SpeakerRepository speakerRepository;
+
+	public SampleService(SpeakerRepository speakerRepository) {
+		this.speakerRepository = speakerRepository;
+	}
+
+
+
 }
