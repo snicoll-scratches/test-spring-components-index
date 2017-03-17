f3cbccf58b1e377379c664be3ec8e1d85f2f2eb2
Stephane Nicoll <snicoll@pivotal.io>
2017-03-17 16:30:29 +0100

    Increase the number of iterations

--- spring-context-indexer-benchmark/bench.sh
+++ spring-context-indexer-benchmark/bench.sh
@@ -4,4 +4,4 @@ mvn clean package
 echo "*****************************"
 echo "* BENCHMARK - SINGLE THREAD *"
 echo "*****************************"
-java -jar target/benchmarks.jar -bm avgt -f 1 -wi 0 -i 3 -r 3s -t 1 -jvmArgs '-server -XX:+AggressiveOpts' .*Benchmark.*
+java -jar target/benchmarks.jar -bm avgt -f 1 -wi 0 -i 10 -r 3s -t 1 -jvmArgs '-server -XX:+AggressiveOpts' .*Benchmark.*
